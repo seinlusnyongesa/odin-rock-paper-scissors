0f986920144bdc1876156d61dc051d698b45abd8
seinlusnyongesa <seinlus044@gmail.com>
2022-09-19 06:12:38 -0700

    player choice, computer choice and find winner

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const selection = ["Rock", "Paper", "Scissors"];
+const selection = ["rock", "paper", "scissors"];
 
-function playRound(playerSelction, computerSelection) {}
+const winning = { scissors: "paper", rock: "scissors", paper: "rock" };
+
+function getComputerChoice() {
+  const selected = Math.floor(Math.random() * selection.length);
+  return selection[selected];
+}
+
+function getPlayerChoice() {
+  let choice = prompt("Enter your choice either, rock, paper or scissors");
+  let choiceToLowercase = choice.toLowerCase();
+
+  return choiceToLowercase;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log("draw");
+    return;
+  }
+
+  if (winning[playerSelection] === computerSelection) {
+    console.log(`you win ${playerSelection} beats ${computerSelection}`);
+  } else {
+    console.log(`you lose ${computerSelection} beats ${playerSelection}`);
+  }
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
